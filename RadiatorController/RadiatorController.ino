c105e339ceafae8fb7ae1fa8aa6acfb8e2af5926
Sebastien Malissard <sebastien.malissard@dbmail.com>
2024-10-03 00:10:34 +0200

    RadiatorController: 1.2.0 Save room name in NVM

--- RadiatorController/RadiatorController.ino
+++ RadiatorController/RadiatorController.ino
@@ -5,18 +5,17 @@
 
 #include "Credentials.h"
 
-// CONFIG SELECTION
-#define ROOM_NAME  "room1"
-//#define ROOM_NAME  "kitchen"
-//#define ROOM_NAME  "bathroom"
-
 // NVM CONFIG
 // Uncomment to write NVM config
 //#define WRITE_NVM_CONFIG
 //#define SERIAL_NUMBER 1
+//#define ROOM_NAME  "room1"
+//#define ROOM_NAME  "bedroom"
+//#define ROOM_NAME  "kitchen"
+//#define ROOM_NAME  "bathroom"
 
 // Firmware version
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
 
 // PINOUT
 #define PIN_SERIAL_TX_DEBUG   1
@@ -31,15 +30,15 @@
 #define CTRL_DISABLE  HIGH
 
 // MQTT
-#define MQTT_TOPIC_PREFFIX                      "home/" ROOM_NAME
-#define MQTT_TOPIC_RAD_FIRMWARE_VERSION         MQTT_TOPIC_PREFFIX"/radiator/firmware_version"
-#define MQTT_TOPIC_RAD_SERIAL_NUMBER            MQTT_TOPIC_PREFFIX"/radiator/serial_number"
-#define MQTT_TOPIC_RAD_SENSOR                   MQTT_TOPIC_PREFFIX"/radiator/sensor"
-#define MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION     MQTT_TOPIC_PREFFIX"/radiator/get_firmware_version"
-#define MQTT_TOPIC_RAD_GET_SERIAL_NUMBER        MQTT_TOPIC_PREFFIX"/radiator/get_serial_number"
-#define MQTT_TOPIC_RAD_SET_SWITCH               MQTT_TOPIC_PREFFIX"/radiator/switch" // Todo need to rename topic with 'set'
-#define MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET   MQTT_TOPIC_PREFFIX"/radiator/set_temperature_offset"
-#define MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET      MQTT_TOPIC_PREFFIX"/radiator/set_humidity_offset"
+#define MQTT_TOPIC_PREFIX                       "home/%s"  // %s replaced by ROOM_NAME
+#define MQTT_TOPIC_RAD_FIRMWARE_VERSION         "/radiator/firmware_version"
+#define MQTT_TOPIC_RAD_SERIAL_NUMBER            "/radiator/serial_number"
+#define MQTT_TOPIC_RAD_SENSOR                   "/radiator/sensor"
+#define MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION     "/radiator/get_firmware_version"
+#define MQTT_TOPIC_RAD_GET_SERIAL_NUMBER        "/radiator/get_serial_number"
+#define MQTT_TOPIC_RAD_SET_SWITCH               "/radiator/switch" // Todo need to rename topic with 'set'
+#define MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET   "/radiator/set_temperature_offset"
+#define MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET      "/radiator/set_humidity_offset"
 #define MQTT_MSG_BUFFER_SIZE                    (50)
 
 // DHT22
@@ -47,30 +46,42 @@
 #define DHT_TYPE  DHT22
 
 // Wifi
-#define WIFI_HOSTNAME ROOM_NAME"-radiator"
+#define WIFI_HOSTNAME "%s-radiator" // %s replaced by ROOM_NAME
 
 #pragma pack(1)
 struct NVMConfig {
   float     sensorTemperatureOffset;
   float     sensorHumidityOffset;
   uint32_t  deviceSerialNumber;
+  char      roomName[32];
 };
-static_assert(sizeof(struct NVMConfig) == 12, "EEPROM config structure size is incorrect");
+static_assert(sizeof(struct NVMConfig) == 4+4+4+32, "EEPROM config structure size is incorrect");
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(DHT_PIN, DHT_TYPE);
 char mqttMsg[MQTT_MSG_BUFFER_SIZE];
 struct NVMConfig config = {};
+String mqttTopicPrefix = "";
 
 #ifdef WRITE_NVM_CONFIG
 void write_nvm_config() {
   Serial.println("WRITE NVM CONFIG:");
-  Serial.print(" - SERIAL_NUMBER: ");
-  Serial.println(SERIAL_NUMBER);
 
   EEPROM.begin(sizeof(NVMConfig));
+
+#ifdef SERIAL_NUMBER
+  Serial.print(" - SERIAL_NUMBER: ");
+  Serial.println(SERIAL_NUMBER);
   EEPROM.put(offsetof(NVMConfig, deviceSerialNumber), SERIAL_NUMBER);
+#endif
+
+#ifdef ROOM_NAME
+  Serial.print(" - ROOM_NAME: ");
+  Serial.println(ROOM_NAME);
+  EEPROM.put(offsetof(NVMConfig, roomName), ROOM_NAME);
+#endif
+
   EEPROM.end();
 }
 #endif
@@ -83,8 +94,11 @@ void setup_wifi()
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
+  char wifi_hostname[64] = {};
+  snprintf(wifi_hostname, sizeof(wifi_hostname)-1, WIFI_HOSTNAME, config.roomName);
+
   WiFi.mode(WIFI_STA);
-  WiFi.hostname(WIFI_HOSTNAME);
+  WiFi.hostname(wifi_hostname);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -116,11 +130,12 @@ void mqtt_reconnect()
     if (client.connect(clientId.c_str(), MQTT_USERNAME, MQTT_PASSWORD))
     {
       Serial.println("connected");
-      client.subscribe(MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION);
-      client.subscribe(MQTT_TOPIC_RAD_GET_SERIAL_NUMBER);
-      client.subscribe(MQTT_TOPIC_RAD_SET_SWITCH);
-      client.subscribe(MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET);
-      client.subscribe(MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET);
+      Serial.println((mqttTopicPrefix + MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION).c_str());
+      client.subscribe((mqttTopicPrefix + MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION).c_str());
+      client.subscribe((mqttTopicPrefix + MQTT_TOPIC_RAD_GET_SERIAL_NUMBER).c_str());
+      client.subscribe((mqttTopicPrefix + MQTT_TOPIC_RAD_SET_SWITCH).c_str());
+      client.subscribe((mqttTopicPrefix + MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET).c_str());
+      client.subscribe((mqttTopicPrefix + MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET).c_str());
     }
     else
     {
@@ -149,17 +164,18 @@ void setup()
 { 
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
 
+  // For serial, don't miss any message
+  delay(2000);
+
+  Serial.println("----------------");
+  Serial.println("Starting...");
+
 #ifdef WRITE_NVM_CONFIG
   write_nvm_config();
 #endif
   EEPROM.begin(sizeof(NVMConfig));
   EEPROM.get(0x00, config);
   EEPROM.end();
-
-  setup_wifi();
-  randomSeed(micros());
-  setup_mqtt();
-  setup_dht();
 
   if (isnan(config.sensorTemperatureOffset)) {
     config.sensorTemperatureOffset = 0;
@@ -177,9 +193,20 @@ void setup()
   Serial.println(config.sensorHumidityOffset);
   Serial.print("Serial number: ");
   Serial.println(config.deviceSerialNumber);
+  Serial.print("Room name: ");
+  Serial.println(config.roomName);
+
+  char str[64] = {};
+  snprintf(str, sizeof(str)-1, MQTT_TOPIC_PREFIX, config.roomName);
+  mqttTopicPrefix = str;
 
   Serial.print("Mqtt topic prefix: ");
-  Serial.println(MQTT_TOPIC_PREFFIX);
+  Serial.println(mqttTopicPrefix);
+
+  setup_wifi();
+  randomSeed(micros());
+  setup_mqtt();
+  setup_dht();
 
   delay(1000);
 
@@ -203,19 +230,21 @@ void mqtt_callback(char* topic, byte* payload, unsigned int len)
   }
   Serial.println();
 
-  if (strcmp(topic, MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION) == 0) {
+  String topicStr(topic);
+
+  if (topicStr == mqttTopicPrefix + MQTT_TOPIC_RAD_GET_FIRMWARE_VERSION) {
     snprintf (mqttMsg, MQTT_MSG_BUFFER_SIZE, "{\"firmware_version\":%s}", VERSION);
     Serial.print("Publish message: ");
     Serial.println(mqttMsg);
-    client.publish(MQTT_TOPIC_RAD_FIRMWARE_VERSION, mqttMsg);
+    client.publish((mqttTopicPrefix + MQTT_TOPIC_RAD_FIRMWARE_VERSION).c_str(), mqttMsg);
   }
-  else if (strcmp(topic, MQTT_TOPIC_RAD_GET_SERIAL_NUMBER) == 0) {
+  else if (topicStr == mqttTopicPrefix + MQTT_TOPIC_RAD_GET_SERIAL_NUMBER) {
     snprintf (mqttMsg, MQTT_MSG_BUFFER_SIZE, "{\"serial_number\":%d,}", config.deviceSerialNumber);
     Serial.print("Publish message: ");
     Serial.println(mqttMsg);
-    client.publish(MQTT_TOPIC_RAD_SERIAL_NUMBER, mqttMsg);
+    client.publish((mqttTopicPrefix + MQTT_TOPIC_RAD_SERIAL_NUMBER).c_str(), mqttMsg);
   }
-  else if (strcmp(topic, MQTT_TOPIC_RAD_SET_SWITCH) == 0) {
+  else if (topicStr == mqttTopicPrefix + MQTT_TOPIC_RAD_SET_SWITCH) {
     if (strncmp((char*) payload, "ON", MIN(len, 2)) == 0)
     {
       Serial.println("Set radiator ON");
@@ -233,7 +262,7 @@ void mqtt_callback(char* topic, byte* payload, unsigned int len)
       Serial.println("Invalid message receive!");
     }
   }
-  else if (strcmp(topic, MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET) == 0) {
+  else if (topicStr == mqttTopicPrefix + MQTT_TOPIC_RAD_SET_TEMPERATURE_OFFSET) {
     char *endptr = nullptr;
     float val = strtof((char*)payload, &endptr);
     Serial.print("Set temperature offset to ");
@@ -248,7 +277,7 @@ void mqtt_callback(char* topic, byte* payload, unsigned int len)
       EEPROM.end();
     }
   }
-  else if (strcmp(topic, MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET) == 0) {
+  else if (topicStr == mqttTopicPrefix + MQTT_TOPIC_RAD_SET_HUMIDITY_OFFSET) {
     char *endptr = nullptr;
     float val = strtof((char*)payload, &endptr);
     Serial.print("Set humidity offset to ");
@@ -289,7 +318,7 @@ void loop_temp()
 
   Serial.print("Publish message: ");
   Serial.println(mqttMsg);
-  client.publish(MQTT_TOPIC_RAD_SENSOR, mqttMsg);
+  client.publish((mqttTopicPrefix + MQTT_TOPIC_RAD_SENSOR).c_str(), mqttMsg);
 }
 
 void loop()
